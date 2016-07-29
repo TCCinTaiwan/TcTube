22a5dbfadf31d723539b72b0be212a94e68b3dad
陳泰澄 <john987john987@gmail.com>
2016-07-29 09:34:58 +0800

    整理上傳內容

--- script/ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-for ($i = 1; $i <= 10; $i++):
-    sleep(1);
-    echo "Count = $i\n";
-    ob_flush();
-    flush();
-endfor;
-?>
--- script/get.php
+++
@@ -1,7 +0,0 @@
-<?
-    for ($i = 1; $i < $argc; $i++) {
-        $get = preg_split("/=/", $argv[$i]);
-        $_GET[$get[0]] = $get[1];
-    }
-    var_dump($_GET);
-?>
