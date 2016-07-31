c363a909d290bf5d3da61d40469cb9b6ac24a135
陳泰澄 <john987john987@gmail.com>
2016-07-31 22:29:23 +0800

    測試檔更新
    test.py

--- test.py
+++ test.py
@@ -3,21 +3,21 @@ import unittest
 import main
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
-        main.app.config['TESTING'] = True
-        self.app = main.app.test_client()
+        main.flaskApplication.config['TESTING'] = True
+        self.flaskApplication = main.flaskApplication.test_client()
     def test_redirect(self):
-        response = self.app.get('/')
+        response = self.flaskApplication.get('/')
         self.assertEquals(response.status, "302 FOUND")
     def test_login_logout(self):
-        response = self.app.post('/login/', data = {
+        response = self.flaskApplication.post('/login/', data = {
             'account': 'test',
             'password': "test",
             'submit': "登入"
         }, follow_redirects = True)
         self.assertEquals(response.status, "200 OK")
-        response = self.app.get('/')
+        response = self.flaskApplication.get('/')
         self.assertEquals(response.status, "200 OK")
-        response = self.app.post('/logout/', data = {
+        response = self.flaskApplication.post('/logout/', data = {
             next: "/"
         }, follow_redirects = True)
         self.assertEquals(response.status, "200 OK")
